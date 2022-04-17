277c5b3b1a9253e4f889e8fbb3b5a431bdb5350b
JakobGlieder <58510986+JakobGlieder@users.noreply.github.com>
2022-04-17 03:50:24 +0200

    Filament Converter

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,23 @@
 <template>
 <div id="container">
-  <img alt="Converter Logo" src="./assets/logo.png">
-  <HelloWorld msg="Converter"/>
-  <div><input type="text" v-model="fromValue" placeholder="enter amount"></div>
-  <div><input type="text" v-model="fromUnit" placeholder="enter current unit"></div>
-  <div>to</div>
-  <div><input type="text" v-model="toUnit" placeholder="enter wanted unit"></div>
+  
+  <img height="100" alt="Converter Logo" src="./assets/logo.png">
 
-  <div><p id="result">{{fromValue}}{{fromUnit}} equals {{result}}{{toUnit}} </p></div>
+  <div><input type="text" v-model="fromValue" placeholder="enter filament weight"> g</div>
+  <div><input type="text" v-model="fromUnit" placeholder="enter filament material"></div>
+  <div>to</div>
+  <div><input type="text" v-model="toUnit" placeholder="enter target material"></div>
+
+  <div v-if="isFinite(meters)"><p id="result">{{fromValue}}g of 1.75mm <span style="text-transform: uppercase"> {{fromUnit}}</span> filament measures</p></div>
+  <div v-if="isFinite(meters)"><p id="meters"><strong>{{meters}}m</strong></p></div>
+  <div v-if="!isFinite(meters)"><p id="metersx">{{meters}}</p></div>
+  <div v-if="isFinite(result)"><p id="result">that corresponds to</p></div>
+  <div v-if="isFinite(result)"><p id="result"><strong>{{result}}g</strong> of <strong style="text-transform: uppercase">{{toUnit}}</strong></p></div>
+  <div v-if="!isFinite(result)"><p id="resultx">{{result}}</p></div>
 </div>
 </template>
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
@@ -20,49 +26,72 @@ export default {
   },
   data: function(){            // the data available to this component (Converter.vue). It must be a function returning an object (to ensure that each component has its own data)
        return{
-         fromValue:1,
+         fromValue:"",
          fromUnit:"",
          toUnit:""
        }
   },
-  components: {
-    HelloWorld
-  },
+  // components: {
+  //   HelloWorld
+  // },
   methods:{
-        toSeconds: function(value,unit){
-            switch(unit){
-              case("s"):
-                  return value;
-              case("min"):
-                  return value*60;
-              case("h"):
-                   return value*60*60;
-              default:
-                   console.log("toMinutes conversion failed - unit incorrect")
-                   return value;
-            }
-        }
+        
   },
 
   computed: {
+      meters: function(){
+        let value = parseFloat(this.fromValue);
+        if(!(isFinite(value))){
+          return "Result will show here after entering values"
+        }
+        switch(this.fromUnit.toUpperCase()){
+          case("PLA"):    return Math.round(100*value * 0.335283)/100;
+          case("ABS"):    return Math.round(100*value * 0.399761)/100;
+          case("ASA"):    return Math.round(100*value * 0.388552)/100;
+          case("PET"):
+          case("PETG"):   return Math.round(100*value * 0.327363)/100;
+          case("NYLON"):  return Math.round(100*value * 0.384955)/100;
+          case("POLYCARBONATE"):
+          case("PC"):     return Math.round(100*value * 0.346459)/100;
+          case("HIPS"):   return Math.round(100*value * 0.388552)/100;
+          case("PVA"):    return Math.round(100*value * 0.349371)/100;
+          case("TPU"):    return Math.round(100*value * 0.346459)/100;
+          case("PMMA"):   return Math.round(100*value * 0.352331)/100;
+          case("COPPER-FILL"):
+          case("COPPERFILL"):    return Math.round(100*value * 0.106603)/100;
+          case(""):
+            return "Please enter base material"
+          default:
+            console.log("First Material not Available")
+            return "Selected base material not available yet, check spelling or suggest material";
+              }
+      },
+
       result: function(){
-            let value = parseFloat(this.fromValue);  // whatever the user types into the <input> Element, it turns out to be a string. We have to convert it to a number(float)
-            if(isFinite(value)){
-                        let valueInSeconds = this.toSeconds(value,this.fromUnit);  // we use "minutes" as an intermediary unit
-                        switch(this.toUnit){
-                        case("s"):
-                            return parseFloat((valueInSeconds).toFixed(5));   // limit to 5 decimals and parseFloat() it to remove redundant 0s
-                        case("min"):
-                            return parseFloat((valueInSeconds/60).toFixed(5));
-                        case("h"):
-                            return parseFloat((valueInSeconds/60/60).toFixed(5));
-                        default:
-                            console.log("Error while converting time - resultUnit not detected!");
-                            return "...";
-                         }
-              
-            }      
-            return "...";  // if the user didn't type in a number
+        let value = parseFloat(this.meters);
+        if(!(isFinite(value))){
+          return "..."
+        }
+        switch(this.toUnit.toUpperCase()){
+          case("PLA"):    return Math.round(100*value / 0.335283)/100;
+          case("ABS"):    return Math.round(100*value / 0.399761)/100;
+          case("ASA"):    return Math.round(100*value / 0.388552)/100;
+          case("PET"):
+          case("PETG"):   return Math.round(100*value / 0.327363)/100;
+          case("NYLON"):  return Math.round(100*value / 0.384955)/100;
+          case("POLYCARBONATE"):
+          case("PC"):     return Math.round(100*value / 0.346459)/100;
+          case("HIPS"):   return Math.round(100*value / 0.388552)/100;
+          case("PVA"):    return Math.round(100*value / 0.349371)/100;
+          case("TPU"):    return Math.round(100*value / 0.346459)/100;
+          case("PMMA"):   return Math.round(100*value / 0.352331)/100;
+          case("COPPER-FILL"):
+          case("COPPERFILL"):    return Math.round(100*value / 0.106603)/100;
+          case(""):
+            return "Please enter target material"
+          default:
+            return "Target material not available yet"
+              }
       }
   }
 
@@ -79,4 +108,16 @@ export default {
   color: #2c3e50;
   margin-top: 12px;
 }
+
+ input{
+    padding:5px;
+    border: 1px solid rgb(0, 0, 0);
+    border-radius:10px;
+    width:15vw;
+    font-size:1em;
+ }
+
+ img{
+    padding:30px;
+ }
 </style>
